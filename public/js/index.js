1d00dd37745d3b71fd9ca127bbb651f96d2f163f
Benjamin Candy <bcandyman93@gmail.com>
2020-01-17 01:23:49 -0800

    Removed unused variables. Changed all single quotations to double quotations.

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,30 @@
-$("#user-create").click(function (event) {
-  const newUserName = $("#username-create").val().trim();
+/* eslint-disable prettier/prettier */
+$("#user-create").click(function() {
+  var newUserName = $("#username-create").val().trim();
 
   // this ensures an empty username cannot be submitted.
   if (newUserName.length === 0) {
-    return
-  };
+    return;
+  }
 
   // ajax post to create a new user.
   $.ajax({
-    url: '/api/user/create',
-    type: 'POST',
+    url: "/api/user/create",
+    type: "POST",
     data: {
       name: newUserName,
       password: "newPassword"
     }
-  }).done((res, rej) => {
-    userId = res
+  }).done(function(res) {
+    userId = res;
 
     // redirect browser to returned route.
     window.location = "/pantry/manage";
   });
 });
 
-$("#user-select").change(function (event) {
+$("#user-select").change(function() {
   userId = this.value;
-  console.log(userId)
+  console.log(userId);
   window.location = "/recipes";
 });
