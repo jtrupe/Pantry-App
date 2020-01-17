e0e89ab0084b0eb37542efb98baeae7183de7cc1
Benjamin Candy <bcandyman93@gmail.com>
2020-01-16 22:44:13 -0800

    Added line to create database 'recipesDB'. Commented out code to create tables.

--- models/schema.sql
+++ models/schema.sql
@@ -1,22 +1,25 @@
 DROP DATABASE IF EXISTS recipesDB;
-USE recipesDB;
+CREATE DATABASE recipesDB;
 
-CREATE TABLE users (
-id INT NOT NULL auto_increment,
-name VARCHAR(100) NOT NULL,
-password VARCHAR(255) NOT NULL,
-PRIMARY KEY (id)
-);
 
-CREATE TABLE masterPantry (
-id INT NOT NULL auto_increment,
-food_name VARCHAR(100) NOT NULL,
-PRIMARY KEY (id)
-);
+-- USE recipesDB;
 
-CREATE TABLE userPantry (
-    id INT NOT NULL AUTO_INCREMENT,
-    food_arr ARRAY,
-    amounts ARRAY
-);
+-- CREATE TABLE users (
+-- id INT NOT NULL auto_increment,
+-- name VARCHAR(100) NOT NULL,
+-- password VARCHAR(255) NOT NULL,
+-- PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE masterPantry (
+-- id INT NOT NULL auto_increment,
+-- food_name VARCHAR(100) NOT NULL,
+-- PRIMARY KEY (id)
+-- );
+
+-- CREATE TABLE userPantry (
+--     id INT NOT NULL AUTO_INCREMENT,
+--     food_arr ARRAY,
+--     amounts ARRAY
+-- );
 
