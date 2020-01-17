885d3789ca8d3100c38b493846bc2b937acfbb55
Benjamin Candy <bcandyman93@gmail.com>
2020-01-17 01:23:09 -0800

    Removed spaces after the function keywords to satisfy the linter.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,42 +1,32 @@
 var db = require("../models");
 
-module.exports = function (app) {
-  app.post("/api/user/create", function (req, res) {
-    db.User.create(req.body).then(function (dbUser) {
-      const userId = dbUser.dataValues.id.toString();
+module.exports = function(app) {
+  app.post("/api/user/create", function(req, res) {
+    db.User.create(req.body).then(function(dbUser) {
+      var userId = dbUser.dataValues.id.toString();
       res.send(userId);
     });
   });
 
-
-
-
-
-
-
-
-
-
-  
-
-
   // Get all examples
-  app.get("/api/examples", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
+  app.get("/api/examples", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function (req, res) {
-    db.Example.create(req.body).then(function (dbExample) {
+  app.post("/api/examples", function(req, res) {
+    db.Example.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function (req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
+  app.delete("/api/examples/:id", function(req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.json(dbExample);
     });
   });
