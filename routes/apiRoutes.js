c6c04ea8c0222000bb086c72d090f405b4c86caf
Benjamin Candy <bcandyman93@gmail.com>
2020-01-17 20:24:44 -0800

    Added axios. Added get route for 'api/ingredient/search'. Added axios call to external api.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,21 @@
 var db = require("../models");
+var axios = require("axios");
 
 module.exports = function(app) {
   app.post("/api/user/create", function(req, res) {
     db.User.create(req.body).then(function(dbUser) {
       var userId = dbUser.dataValues.id.toString();
       res.send(userId);
+    });
+  });
+
+  app.get("/api/ingredient/search", function(req, res) {
+    var url = "https://api.spoonacular.com/food/ingredients/autocomplete";
+    var query = "?query=" + req.query.ingredientName;
+    var numResults = "&number=" + "5";
+    var apiKey = "&apiKey=" + "e4111bae48a3477c90977f63b0c2d882";
+    axios.get(url + query + numResults + apiKey).then(function(response){
+      res.send(response.data);
     });
   });
 
