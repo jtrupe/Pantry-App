50d5b0840da64138d469ed2202aaedbf861a7b96
Benjamin Candy <bcandyman93@gmail.com>
2020-01-19 11:30:14 -0800

    Added 'assembleIngredientString' function. Added '/recipes/pantry' route. Added 404 route to display route was not found.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,45 +1,57 @@
 var db = require("../models");
+var axios = require("axios");
 
-    // !!THIS ARRAY TO BE DELETED ONCE SQL DATABASE IS SET UP!!
-    // !!USE ONLY FOR TESTING PURPOSES!!
-    var dummyPantryData = [
-      {
-        id:"2",
-        name:"egg",
-        createdAt: "2020-01-16T22:36:17.000Z",
-        updatedAt: "2020-01-16T22:36:17.000Z"
-      },
-      {
-        id:"4",
-        name:"milk",
-        createdAt: "2020-01-16T22:36:17.000Z",
-        updatedAt: "2020-01-16T22:36:17.000Z"
-      },
-      {
-        id:"7",
-        name:"flour",
-        createdAt: "2020-01-16T22:36:17.000Z",
-        updatedAt: "2020-01-16T22:36:17.000Z"
-      },
-      {
-        id:"9",
-        name:"apple juice",
-        createdAt: "2020-01-16T22:36:17.000Z",
-        updatedAt: "2020-01-16T22:36:17.000Z"
-      },
-      {
-        id:"10",
-        name:"butter",
-        createdAt: "2020-01-16T22:36:17.000Z",
-        updatedAt: "2020-01-16T22:36:17.000Z"
-      },
-      {
-        id:"12",
-        name:"broccoli",
-        createdAt: "2020-01-16T22:36:17.000Z",
-        updatedAt: "2020-01-16T22:36:17.000Z"
-      }
-    ]
+function assembleIngredientString(data) {
+  var ingredientString = "";
+  data.forEach(function (val, ind) {
+    if (ind !== 0) {
+      ingredientString += ",";
+    }
+    ingredientString += val.name;
+  });
+  return ingredientString
+}
+
+// !!THIS ARRAY TO BE DELETED ONCE SQL DATABASE IS SET UP!!
+// !!USE ONLY FOR TESTING PURPOSES!!
+var dummyPantryData = [
+  {
+    id: "2",
+    name: "egg",
+    createdAt: "2020-01-16T22:36:17.000Z",
+    updatedAt: "2020-01-16T22:36:17.000Z"
+  },
+  {
+    id: "4",
+    name: "milk",
+    createdAt: "2020-01-16T22:36:17.000Z",
+    updatedAt: "2020-01-16T22:36:17.000Z"
+  },
+  {
+    id: "7",
+    name: "flour",
+    createdAt: "2020-01-16T22:36:17.000Z",
+    updatedAt: "2020-01-16T22:36:17.000Z"
+  },
+  {
+    id: "9",
+    name: "apple juice",
+    createdAt: "2020-01-16T22:36:17.000Z",
+    updatedAt: "2020-01-16T22:36:17.000Z"
+  },
+  {
+    id: "10",
+    name: "butter",
+    createdAt: "2020-01-16T22:36:17.000Z",
+    updatedAt: "2020-01-16T22:36:17.000Z"
+  },
+  {
+    id: "12",
+    name: "broccoli",
+    createdAt: "2020-01-16T22:36:17.000Z",
+    updatedAt: "2020-01-16T22:36:17.000Z"
+  }
+]
 
 module.exports = function (app) {
 
@@ -50,31 +62,52 @@ module.exports = function (app) {
     });
   });
 
+  // Redirect '/' route to '/login'
+  app.get("/", function (req, res) {
+    res.redirect("/login")
+  });
+  
   // This route should display all ingredients of the user
   app.get("/pantry/manage", function (req, res) {
     res.render('pantry', { title: "Pantry", dummyPantryData });
   });
-
+  
   // This route should display recipes that the user currently has in their database
-  app.get("/recipes", function (req, res) {
-    res.render('recipes', { title: "Recipes" });
+  app.get("/recipes/pantry", function (req, res) {
+    var url =
+    "https://api.spoonacular.com/recipes/findByIngredients?ingredients=";
+    var ingredients = assembleIngredientString(dummyPantryData)
+    var numResults = "&number=" + "5";
+    var instructions = "&instructionsRequired=true";
+    var apiKey = "&apiKey=" + process.env.SPOONACULAR_KEY;
+    
+    axios
+    .get(url + ingredients + numResults + instructions + apiKey)
+    .then(function (response) {
+      var data = response.data
+      res.render('recipes', { title: "Recipes", header: "Recipes by Pantry", data });
+    });
   });
-
+  
   // this route should update the quantity of an ingredient item in the database
-  app.put("/pantry/ingredient/update", function(req, res){
+  app.put("/pantry/ingredient/update", function (req, res) {
     var itemId = req.body.ingredientId;
     console.log("Updating quantity of itemId: " + itemId);
-    setTimeout(function() {
-      res.send({redirect: "/pantry/manage"});
+    setTimeout(function () {
+      res.send({ redirect: "/pantry/manage" });
+    }, 1000);
+  });
+  
+  // this route should remove an ingredient item in the database
+  app.delete("/pantry/ingredient/remove", function (req, res) {
+    var itemId = req.body.ingredientId;
+    console.log("Removing itemId: " + itemId);
+    setTimeout(function () {
+      res.send({ redirect: "/pantry/manage" });
     }, 1000);
   });
 
-  // this route should remove an ingredient item in the database
-  app.delete("/pantry/ingredient/remove", function(req, res){
-    var itemId = req.body.ingredientId;
-    console.log("Removing itemId: " + itemId );
-    setTimeout(function() {
-      res.send({redirect: "/pantry/manage"});
-    }, 1000);
+  app.get("/*", function (req, res) {
+    res.render("404")
   });
 };
