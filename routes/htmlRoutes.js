dfc49e94e0bc0182b1a9a26cbab36708b8877584
Benjamin Candy <bcandyman93@gmail.com>
2020-01-18 23:40:30 -0800

    Added '/pantry/ingredient/update' put route. Added '/pantry/ingredient/remove' delete route. Removed example code.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,7 @@
 var db = require("../models");
 
-module.exports = function (app) {
-  // This is the route to log in the user.
-  app.get("/login", function (req, res) {
-    db.User.findAll({}).then(function (dbUsers) {
-      res.render('index', { title: "Login", dbUsers });
-    });
-  });
-
-  app.get("/pantry/manage", function (req, res) {
-
+    // !!THIS ARRAY TO BE DELETED ONCE SQL DATABASE IS SET UP!!
+    // !!USE ONLY FOR TESTING PURPOSES!!
     var dummyPantryData = [
       {
         id:"2",
@@ -48,36 +40,41 @@ module.exports = function (app) {
         updatedAt: "2020-01-16T22:36:17.000Z"
       }
     ]
+
+module.exports = function (app) {
+
+  // This is the route to log in the user.
+  app.get("/login", function (req, res) {
+    db.User.findAll({}).then(function (dbUsers) {
+      res.render('index', { title: "Login", dbUsers });
+    });
+  });
+
+  // This route should display all ingredients of the user
+  app.get("/pantry/manage", function (req, res) {
     res.render('pantry', { title: "Pantry", dummyPantryData });
   });
 
+  // This route should display recipes that the user currently has in their database
   app.get("/recipes", function (req, res) {
     res.render('recipes', { title: "Recipes" });
   });
 
-// Load index page
-  app.get("/", function (req, res) {
-    db.Example.findAll({}).then(function (dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+  // this route should update the quantity of an ingredient item in the database
+  app.put("/pantry/ingredient/update", function(req, res){
+    var itemId = req.body.ingredientId;
+    console.log("Updating quantity of itemId: " + itemId);
+    setTimeout(function() {
+      res.send({redirect: "/pantry/manage"});
+    }, 1000);
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function (req, res) {
-    res.render("404");
+  // this route should remove an ingredient item in the database
+  app.delete("/pantry/ingredient/remove", function(req, res){
+    var itemId = req.body.ingredientId;
+    console.log("Removing itemId: " + itemId );
+    setTimeout(function() {
+      res.send({redirect: "/pantry/manage"});
+    }, 1000);
   });
 };
