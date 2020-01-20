422481a520c027edc50aa0cf65335f1f79ccc0aa
Benjamin Candy <bcandyman93@gmail.com>
2020-01-20 15:01:33 -0800

    Added timeout method to reduce the amount of api calls when autopopulating fields.

--- public/js/pantry.js
+++ public/js/pantry.js
@@ -1,32 +1,37 @@
+var ingredientSearchTimeout;
+
 $("#ingredient-search").keyup(function() {
+  function getAutofillData() {
+    $.ajax({
+      url: "/api/ingredient/search",
+      type: "GET",
+      data: { ingredientName: itemToSearch }
+    }).done(function(res) {
+      $(".returned-search-items").empty();
+      res.forEach(function(val) {
+        console.log(val.name);
+        var newDiv = $("<div>");
+        var itemName = $("<p>").text(val.name);
+        var addBtn = $("<button>").text("Add Item");
+        newDiv.append(itemName);
+        newDiv.append(addBtn);
+        $(".returned-search-items").append(newDiv);
+      });
+    });
+  }
+
+  clearTimeout(ingredientSearchTimeout);
+
   var itemToSearch = $("#ingredient-search")
     .val()
     .trim();
-
   $(".returned-search-items").empty();
 
   // this ensures an empty itemToSearch cannot be queried to the API
   if (itemToSearch.length === 0) {
     return;
   }
-
-  // ajax get to find ingredient matches.
-  $.ajax({
-    url: "/api/ingredient/search",
-    type: "GET",
-    data: { ingredientName: itemToSearch }
-  }).done(function(res) {
-    $(".returned-search-items").empty();
-    res.forEach(function(val) {
-      console.log(val.name);
-      var newDiv = $("<div>");
-      var itemName = $("<p>").text(val.name);
-      var addBtn = $("<button>").text("Add Item");
-      newDiv.append(itemName);
-      newDiv.append(addBtn);
-      $(".returned-search-items").append(newDiv);
-    });
-  });
+  ingredientSearchTimeout = setTimeout(getAutofillData, 500);
 });
 
 // event listener to alter quantity of pantry item
