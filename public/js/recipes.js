14dd7d872998af285430df1b7e95b9d00a489edb
Benjamin Candy <bcandyman93@gmail.com>
2020-01-26 01:19:21 -0800

    Updated styling

--- public/js/recipes.js
+++ public/js/recipes.js
@@ -1,37 +1,49 @@
-$("#search-recipe-name").click(function() {
+$("#search-recipe-name").click(function () {
   var recipeName = $("#recipe-name")
     .val()
     .trim();
   window.location = "/recipes/search/name/" + recipeName;
 });
 
-$(".recipe-item").click(function() {
+$(".recipe-item").click(function () {
   var recipeId = $(this).attr("data-recipe-id");
   window.location = "/recipe/details/" + recipeId;
 });
 
-$("#add-ingredient").click(function() {
+$("#add-ingredient").click(function () {
   var newDiv = $(".ingredient-item-div:first").clone();
   newDiv.find("input").val("");
   $(".search-ingredients").append(newDiv);
 });
 
-$(document).on("click", ".ingredient-item-remove", function() {
+$(document).on("click", ".ingredient-item-remove", function () {
+  // console.log(this);
+  // console.log(
+  //   $(this)
+  //     .parent()
+  //     .parent()
+  //     .parent()
+  // );
+
   if ($(".ingredient-item-div").length - 1 === 0) {
     $(this)
+      .parent()
+      .parent()
       .parent()
       .find("input")
       .val("");
   } else {
     $(this)
       .parent()
+      .parent()
+      .parent()
       .remove();
   }
 });
 
-$("#search-recipe-ingredients").click(function() {
+$("#search-recipe-ingredients").click(function () {
   var ingredientsArr = [];
-  $(".ingredient-item").each(function(index, element) {
+  $(".ingredient-item").each(function (index, element) {
     ingredientsArr.push(element.value);
   });
   window.location = "/recipes/search/ingredients/" + ingredientsArr.join();
