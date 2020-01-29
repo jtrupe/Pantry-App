83ab996aa5735fd37dd49a51e73a2253c906931c
Chase Tsutsui <chasetsutsui@yahoo.com>
2020-01-28 18:41:34 -0800

    authentication working

--- models/user.js
+++ models/user.js
@@ -9,13 +9,6 @@ module.exports = function(sequelize, DataTypes) {
           len: [1]
         }
       },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [1]
-        }
-      },
       password: {
         type: DataTypes.STRING,
         allowNull: false,
